b114f7fab52d1dbd178b091522b33730a7f7e7be
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 21:30:19 -0500

    Se implementa la clase Framework de acuerdo al video 19 con una clave foranea. La implementacion se realizo en example/models.py

--- example/models.py
+++ example/models.py
@@ -21,4 +21,4 @@ class Language(models.Model):
 
 class Framework(models.Model):
     name= models.CharField(max_length=10)
-    language = models.ForeignKey
+    language = models.ForeignKey(Language,on_delete=models.CASCADE)
