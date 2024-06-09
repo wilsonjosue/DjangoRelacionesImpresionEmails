b940604aa68d8957f2e781584ee3827df4d47085
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 21:17:18 -0500

    Se implementa dos modelos, las clases Simple y DateExample deacuerdo al video 19. La implementacio se realizo en example/models.py

--- example/models.py
+++ example/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Simple(models.Model):
+    text = models.CharField(max_length= 10)
+    numbre = models.IntegerField(null= True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
+
+class DateExample(models.Model):
+    the_date = models.DateTimeField()
+
+class NullExample(models.Model):
+    col = models.CharField()
+
+
+
+
