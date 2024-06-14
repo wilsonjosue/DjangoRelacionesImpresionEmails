bd329f3feb834ea3d2be8442eb4d5018959b3251
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-14 17:33:38 -0500

    Se modifica el tamaño de los campos name. Cambio realizado en example/models.py

--- example/models.py
+++ example/models.py
@@ -30,13 +30,13 @@ class Framework(models.Model):
         return self.name
 
 class Movie(models.Model):
-    name= models.CharField(max_length=10)
+    name= models.CharField(max_length=15)
 
     def __str__(self):
         return self.name
     
 class Character(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
     movies = models.ManyToManyField(Movie)
 
     def __str__(self):
