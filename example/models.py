83d9ebc96d477ae910d9d719903ffd4daa5d81a0
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-14 15:49:49 -0500

    Se agrega la funcion str en la clase Language y Framework para que retoenen el nombre. Cambio realizado en example/models.py

--- example/models.py
+++ example/models.py
@@ -19,6 +19,12 @@ class NullExample(models.Model):
 class Language(models.Model):
     name= models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 class Framework(models.Model):
     name= models.CharField(max_length=10)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
