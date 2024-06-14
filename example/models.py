1f9ddcd0e34cd4d685c9c17b19f58099a9e2728a
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-14 18:20:40 -0500

    Se crea y aplica las migraciones para corregir el tamaño de los campos de name version 2. Cambio realizado en example/migrations/

--- example/models.py
+++ example/models.py
@@ -23,14 +23,14 @@ class Language(models.Model):
         return self.name
 
 class Framework(models.Model):
-    name= models.CharField(max_length=10)
+    name= models.CharField(max_length=20)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Movie(models.Model):
-    name= models.CharField(max_length=15)
+    name= models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
