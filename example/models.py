f55d8048fe05759f58feac918f581c7c930d5359
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-14 16:20:37 -0500

    Se agrega la clase Movie mas su funcion str. Cambio realizado en example/models.py

--- example/models.py
+++ example/models.py
@@ -28,3 +28,10 @@ class Framework(models.Model):
 
     def __str__(self):
         return self.name
+
+class Movie(models.Model):
+    name= models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    
